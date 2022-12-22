895a23645845d2699ad51e9bc72d792dd29effea
Jerry Han <43237739+JRTWCA@users.noreply.github.com>
2022-12-22 16:59:58 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace RFID_Scan_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.IDs = new System.Windows.Forms.Label();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(280, 7);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(373, 7);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BaudRate";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AllowDrop = true;
+            this.numericUpDown1.Location = new System.Drawing.Point(79, 20);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            57600,
+            0,
+            0,
+            0});
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 323);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(717, 225);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(735, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Unique IDs";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(14, 43);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(66, 13);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Data Stream";
+            // 
+            // IDs
+            // 
+            this.IDs.AutoSize = true;
+            this.IDs.Location = new System.Drawing.Point(735, 92);
+            this.IDs.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.IDs.Name = "IDs";
+            this.IDs.Size = new System.Drawing.Size(13, 13);
+            this.IDs.TabIndex = 8;
+            this.IDs.Text = "0";
+            // 
+            // SerialPort
+            // 
+            this.SerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort_DataReceived);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 3);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Port #";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 19);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 7);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 33);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(16, 58);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(716, 238);
+            this.listBox2.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(465, 7);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 33);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Clear ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 307);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Sorted Data";
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(738, 287);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 33);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Save Histogram";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(738, 326);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Backwards";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 567);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.IDs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label IDs;
+        private System.IO.Ports.SerialPort SerialPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RFID_Scan_Monitor
+{
+
+    public partial class Form1 : Form
+    {
+        LinkedList<string> rawlist = new LinkedList<string>();
+        LinkedList<LinkedListNode<RFTag>> taglist = new LinkedList<LinkedListNode<RFTag>>();
+        LinkedList<int> histogram = new LinkedList<int>();
+        SaveFileDialog logFile;
+        StreamWriter sw,sw2;
+        string cutoff = String.Empty;
+        private readonly object listLock = new object();
+        StringBuilder sb = new StringBuilder();
+        char LF = (char)10;
+
+
+        bool headerRead = true;
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            Font replacementFont = new Font("Arial", 30, FontStyle.Bold);
+            IDs.Font = replacementFont;
+            replacementFont = new Font("Arial", 20, FontStyle.Bold);
+            label2.Font = replacementFont;
+            getports();  
+        }
+
+
+         
+        void getports()
+        {
+            String[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+        LinkedListNode<RFTag> getTag(LinkedList<LinkedListNode<RFTag>> list,string TagID)
+        {
+            if (list.Any() == false) 
+            { 
+                return null;
+            }
+            TagID = TagID.Trim('\n');
+            foreach (LinkedListNode<RFTag> tag in list)
+            {
+                if (TagID.Contains(tag.Value.EPC))
+                {
+                    return tag;
+                }
+            }
+            return null;
+        }
+        private void PrintTagList (LinkedList<LinkedListNode<RFTag>> list)
+        {
+            string currentDate = DateTime.Today.ToString("ddMMyyyy");
+            logFile = new SaveFileDialog();
+            logFile.FileName = "Rf_Log_" + currentDate + ".txt";
+            logFile.Filter = "Text Files | *.txt";
+            if (logFile.ShowDialog() == DialogResult.OK)
+            {
+                sw2 = new StreamWriter(logFile.FileName);
+                AddToListBox("Printing Tag List: Size =" + list.Count);
+                sw2.WriteLine("Printing Tag List: Size =" + list.Count);
+                AddToListBox("EPC                                             Total#\t\tA1\tA2\tA3");
+                sw2.WriteLine("EPC                           Total#\tA1\tA2\tA3");
+                if (list.Any() == false)
+                {
+                    return;
+                }
+                foreach (LinkedListNode<RFTag> tag in list)
+                {
+                    AddToListBox(tag.Value.EPC + "\t" + tag.Value.readcount + "\t" + tag.Value.antcount[0] + "\t" + tag.Value.antcount[1] + "\t" + tag.Value.antcount[2]);
+                    sw2.Write(tag.Value.EPC + "\t" + tag.Value.readcount + "\t" + tag.Value.antcount[0] + "\t" + tag.Value.antcount[1] + "\t" + tag.Value.antcount[2]);
+                }
+                sw2.Close();
+            }
+            return;
+        }
+        private void AddToListBox(string oo)
+        {
+            Invoke(new MethodInvoker(
+                           delegate { listBox1.Items.Add(oo); }
+                           ));
+        }
+        private void AddToListBox2(string oo)
+        {
+            Invoke(new MethodInvoker(
+                           delegate { listBox2.Items.Add(oo); }
+                           ));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // This event handler is where the time-consuming work is done.
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            int i = 0;
+            int hist;
+            while (true) 
+            { 
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Perform a time consuming operation and report progress.
+                    //System.Threading.Thread.Sleep(50);
+                    lock (listLock)
+                    {
+                        if (rawlist.Any())
+                        {
+                            string s = rawlist.First();
+                            rawlist.RemoveFirst();
+                            s = s.Trim('\n');
+                            
+                                var data = s.Split(new[] { ',' });
+                                if (data.Length == 8)
+                                {
+                                    AddToListBox2(data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7]);
+
+                                    LinkedListNode<RFTag> T;
+                                    T = getTag(taglist, data[0]);
+                                    if (T == null)
+                                    {
+                                        RFTag tag = new RFTag(data[0], 0, 0, Int32.Parse(data[3]), 0, 0, 0, "0");
+                                        tag.antcount[Int32.Parse(data[2]) - 1] = Int32.Parse(data[3]);
+                                        T = new LinkedListNode<RFTag>(tag);
+                                        taglist.AddLast(T);
+                                        i++;
+                                        hist = int.Parse(DateTime.Now.ToString("hhmmss"));
+                                        hist += (Int32.Parse(data[2])-1) * 1000000;
+                                        hist += Int32.Parse(data[3]) * 10000000;
+                                        if (checkBox1.Checked)
+                                        {
+                                            hist += 100000000;
+                                        }
+                                        histogram.AddLast(hist);
+                                        worker.ReportProgress(i);
+
+                                    }
+                                    else
+                                    {
+                                        T.Value.readcount += Int32.Parse(data[3]);
+                                        T.Value.antcount[Int32.Parse(data[2]) - 1] += Int32.Parse(data[3]);
+                                    }
+                                    cutoff = String.Empty;
+                                }
+                        }
+                    }
+                }
+            }
+        }
+
+        // This event handler updates the progress.
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            IDs.Text = e.ProgressPercentage.ToString();
+        }
+
+        // This event handler deals with the results of the background operation.
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                label4.Text = "Tags Sorted";
+            }
+            else if (e.Error != null)
+            {
+                label4.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                resultLabel.Text = "Done Processing!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string currentDate = DateTime.Today.ToString("ddMMyyyy");
+
+            logFile = new SaveFileDialog();
+            logFile.FileName = "Rf_Log_Raw_" + currentDate + ".txt";
+            logFile.Filter = "Text Files | *.txt";
+
+            if (logFile.ShowDialog() == DialogResult.OK)
+            {
+                if (!headerRead)
+                {
+                    resultLabel.Text = "Started Scan: Waiting for Header";
+                    AddToListBox2("Started Scan: Waiting for Header");
+                    AddToListBox2("");
+                }
+                else
+                {
+                    resultLabel.Text = "Started Scan: Skipping Header";
+                    AddToListBox2("Started Scan: Skipping Header");
+                    AddToListBox2("");
+                }
+                button2.Enabled = true;
+                button1.Enabled = false;
+                sw = new StreamWriter(logFile.FileName);
+
+                if (backgroundWorker1.IsBusy != true)
+                {
+                    // Start the asynchronous operation.
+                    backgroundWorker1.RunWorkerAsync();
+                    label4.Text = "Processing Data";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            headerRead = false;
+            SerialPort.Close();
+            sw.Close();
+            PrintTagList(taglist);
+
+            resultLabel.Text = "Stopped";
+            button1.Enabled = true;
+            button3.Enabled = true;
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            System.IO.Ports.SerialPort sp = (System.IO.Ports.SerialPort)sender;
+            string s;
+            string indata = sp.ReadExisting();
+
+            if (sw != null)
+            {
+                sw.Write(indata);
+            }
+            if (headerRead)
+            {
+
+                foreach (char c in indata)
+                {
+                    if (c == LF)
+                    {
+                        sb.Append(c);
+
+                        s = sb.ToString();
+                        sb.Clear();
+                        lock (listLock)
+                        {
+                            rawlist.AddLast(s);
+                        }
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+            }
+            else
+            {
+                AddToListBox2(indata);
+            }
+            if (!headerRead && indata.Contains("PHASE, mV"))
+            {
+                headerRead = true;
+                Invoke(new MethodInvoker(
+                           delegate { resultLabel.Text = "Started Scan: Header Found"; }
+                           ));
+                AddToListBox2("Header Found");
+                AddToListBox2("");
+                AddToListBox2("Incoming Tags");
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try 
+            {
+                if (comboBox1.Text == "" || numericUpDown1.Value == 0)
+                {
+                    resultLabel.Text = "Please select port settings";
+                }
+                else
+                {
+                    SerialPort.PortName = comboBox1.Text;
+                    SerialPort.BaudRate = (int)numericUpDown1.Value;
+                    resultLabel.Text = "Connected";
+                    AddToListBox2("Connected");
+                    SerialPort.Open();
+                    button3.Enabled = false;
+                    button1.Enabled = true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                resultLabel.Text = "Connection Error: " + ex.Message;
+            }
+           
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string currentDate = DateTime.Today.ToString("ddMMyyyy");
+
+            logFile = new SaveFileDialog();
+            logFile.FileName = "Rf_Histogram" + currentDate + ".txt";
+            logFile.Filter = "Text Files | *.txt";
+
+            if (logFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter w = new StreamWriter(logFile.FileName);
+                foreach (int time in histogram)
+                {
+                    w.WriteLine(time);
+                }
+                w.Close();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+    }
+    class RFTag
+    {
+        public string EPC;
+        public int protocol;
+        public int antenna;
+        public int readcount;
+        public int RSSI;
+        public int freq;
+        public int phase;
+        public string mv;
+        public int[] antcount = new int[3];
+        public RFTag(string id, int prot, int ant, int cnt, int rs, int fre, int pha, string v)
+        {
+            EPC = id;
+            protocol = prot;
+            antenna = ant;
+            readcount = cnt;
+            RSSI = rs;
+            freq = fre;
+            phase = pha;
+            mv = v;
+            
+        }
+    }
+
+}
